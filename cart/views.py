267905964e2e4b72e9208d346ef9b8e2326a21e2
tnka <tnka@xyz.com>
2025-06-29 02:10:22 -0300

    feat(cart): new files

---
+++ cart/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import View, TemplateView
+
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+class CartAddView(View):
+    def post(self, request, *args, **kwargs):
+        cart = Cart(request)
+        product_id = kwargs.get('product_id')
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddProductForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            cart.add(
+                product=product,
+                quantity=cd['quantity'],
+                override_quantity=cd['override']
+            )
+        return redirect('cart:cart_detail')
+
+
+class CartRemoveView(View):
+    def post(self, request, *args, **kwargs):
+        cart = Cart(request)
+        product_id = kwargs.get('product_id')
+        product = get_object_or_404(Product, id=product_id)
+        cart.remove(product)
+        return redirect('cart:cart_detail')
+
+
+class CartDetailView(TemplateView):
+    template_name = 'cart/detail.html'
+
+    def get_context_data(self, **kwargs):
+        cart = Cart(self.request)
+        for item in cart:
+            item['update_quantity_form'] = CartAddProductForm(
+                initial={'quantity': item['quantity'], 'override': True}
+            )
+        kwargs.update({'cart': cart})
+        return kwargs
