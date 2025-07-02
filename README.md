ceded45bd2c788082d83808fa178a4afe42e8826
tnka <tnka@xyz.com>
2025-07-01 23:07:08 -0300

    feat: add stripe webhook

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # Django E-Commerce Project
 
-### Commands
+## Commands
 
+### Docker 
 1. docker pull rabbitmq:3.10-management
 2. docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.10-management 
-3. celery -A config worker -l info
-4. celery -A config flower --basic-auth=username:password
-5. py manage.py runserver
 
-### Servers
+
+### Celery
+1. celery -A config worker -l info
+2. celery -A config flower --basic-auth=username:password (OPTIONAL)
+
+### Stripe
+1. stripe login
+2. stripe listen --forward-to 127.0.0.1:8000/payment/webhook/
+
+### Django
+
+1. python3 manage.py runserver
+
+## Servers
 
 - **127.0.0.1:8000** Django Site
 - **127.0.0.1:15672** RabbitMQ Interface
-- **127.0.0.1:5555** Celery Interface
+- **127.0.0.1:5555** Celery Interface (OPTIONAL)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -10,6 +11,7 @@ class Order(models.Model):
     city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    stripe_id = models.CharField(max_length=250, blank=True)
     paid = models.BooleanField(default=False)
 
     class Meta:
@@ -23,6 +25,18 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            # no payment associated
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            # Stripe path for test payments
+            path = '/test/'
+        else:
+            # Stripe path for real payments
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
 
 
 class OrderItem(models.Model):
